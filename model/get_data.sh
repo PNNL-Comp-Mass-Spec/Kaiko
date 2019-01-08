e9b4717a4229c5e6fdec18d23f9f762099fd22b4
Ming Wang <mwang87@gmail.com>
2019-01-08 12:26:54 -0800

    get data for models

--- model/get_data.sh
+++ model/get_data.sh
@@ -1,5 +1,7 @@
 # Downloading Kaiko Pre-trained model
+wget ftp://massive.ucsd.edu/MSV000083150/updates/2019-01-08_mwang87_5b6489dc/other/model/checkpoint
+wget ftp://massive.ucsd.edu/MSV000083150/updates/2019-01-08_mwang87_5b6489dc/other/model/translate.ckpt-2123500.data-00000-of-00001
+wget ftp://massive.ucsd.edu/MSV000083150/updates/2019-01-08_mwang87_5b6489dc/other/model/translate.ckpt-2123500.index
 
 # Downloading knapsack
-
-wget https://drive.google.com/open?id=1um832lwCDpBptkeeCFhzWYuK4tgRwdm_
+wget ftp://massive.ucsd.edu/MSV000083150/updates/2019-01-08_mwang87_5b6489dc/other/model/knapsack.npy
